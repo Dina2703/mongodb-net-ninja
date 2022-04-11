6dfb5b8492b8b3847b286a5e75036b0051d859eb
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-11 12:25:11 -0400

    calling our two database functions from app.js (express file)

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 //this returts us a function that we store in 'express' constant
 const express = require("express");
+const db = require("./db");
+
+// importing the exported functions from db.js
+const { connectToDb, getDb } = require("./db");
 
 //init app & middleware
 //to initialize the express app we have to invoke this 'express' function.
 const app = express();
 
-//set listen port for our express app.
-app.listen(3000, () => {
-  console.log("app listening on port 3000");
+//db connection
+//we want to connect to the mongodb database right away before we started listening for requests to this api. When we call our first function for inital connection we need to pass argument, that we called as 'callback' in db.js.
+let db;
+connectToDb((err) => {
+  if (!err) {
+    //set listen port for our express app.
+    app.listen(3000, () => {
+      console.log("app listening on port 3000");
+    });
+    //getDb() returs us the database connection object that we need, and we store it in 'db', now we can interact with  database via 'db' const.
+    db = getDb();
+  }
 });
 
 //routes
